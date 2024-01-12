bed781eaea623b8c9cb4c78c1cd0830de22a4e1c
Augusto215 <augusto.dataanalysis@gmail.com>
2024-01-12 10:31:55 -0300

    novo21

--- reservaApp/settings.py
+++ reservaApp/settings.py
@@ -39,10 +39,10 @@ PASSWORD_RESET_TEMPLATE_NAME = 'core/mudarSenha.html'
 LOGIN_URL = 'login'
 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['reservajaqueira.onrender.com','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://reservajaqueira.onrender.com/*', 'http://127.0.0.1/*']
+ALLOWED_HOSTS = ['reservajaqueira.onrender.com','127.0.0.1', 'www.reservapataxodajaqueira.com.br', 'reservapataxodajaqueira.com.br']
+CSRF_TRUSTED_ORIGINS = ['https://reservajaqueira.onrender.com/*', 'http://127.0.0.1/*', 'https://www.reservapataxodajaqueira.com.br/*', 'https://reservapataxodajaqueira.com.br/*' ]
 
 
 AUTHENTICATION_BACKENDS = [
